6e72235270de87e50da4540c064b88588344a5bd
baldx <mr.ahmad.m2006@gmail.com>
2023-06-10 19:50:20 +0200

    reveal board script

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const playButton = () => {
     playBtn = document.querySelector(".play");
+    board = document.querySelector(".board");
+    body = document.querySelector("body");
 
     playBtn.onclick = () => {
         firstPage = document.querySelector(".first-page");
         firstPage.style.display = "none";
+
+        board.style.display = "grid";
+        body.style["justify-content"] = "center";
     }
 }
 
