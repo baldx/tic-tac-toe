75aeceed057455e695e84edf2b1d8597e4e048eb
baldx <mr.ahmad.m2006@gmail.com>
2023-06-11 14:42:08 +0200

    create script for player turn

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-let gameBoard = [];
-let winner = null;
+let gameBoard = (() => {
+    let winner = null;
+    let turn = 0;
+    let board = [];
+
+    const createPlayer = (name, mark, turn) => {
+        return {name, mark, turn};
+    }
+
+    const player1 = createPlayer("player1", "X", true);
+    const player2 = createPlayer("player2", "O", false);
+
+    const playerTurn = (() => {
+        let square = document.querySelectorAll(".square");
+
+        square.forEach(square => {
+            square.addEventListener("click", element => {
+                if (player1.turn == true && square.innerHTML == "") {
+                    board[element.id] = player1.mark;
+                    square.textContent = player1.mark;
+                    player1.turn = false;
+                    player2.turn = true;
+                }
+                else if (player2.turn == true && square.innerHTML == "") {
+                    board[element.id] = player2.mark;
+                    square.textContent = player2.mark;
+                    player1.turn = true;
+                    player2.turn = false;
+                }
+                else return;
+            })
+        })
+    })
+    playerTurn();
+    
+})();
 
 const playButton = (() => {
     playBtn = document.querySelector(".play");
@@ -13,15 +47,4 @@ const playButton = (() => {
         board.style.display = "grid";
         body.style["justify-content"] = "center";
     }
-})();
-
-const placeMark = (() => {
-    let square = document.querySelectorAll(".square");
-
-        square.forEach(element => {
-        element.addEventListener("click", () => {
-            element.innerHTML = "X";
-        })
-    })
-    
 })();
