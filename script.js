966b64f804e95267392baab98151b95403c53fcc
baldx <mr.ahmad.m2006@gmail.com>
2023-06-11 14:50:19 +0200

    add array with win possibilities

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ let gameBoard = (() => {
     let winner = null;
     let turn = 0;
     let board = [];
+    let winPossibilities = [
+        [0, 1, 2],
+        [0, 3, 6],
+        [0, 4, 8],
+        [1, 4, 7],
+        [2, 5, 8],
+        [2, 4, 6],
+        [3, 4, 5],
+        [6, 7, 8]
+    ];
 
     const createPlayer = (name, mark, turn) => {
         return {name, mark, turn};
@@ -18,12 +28,16 @@ let gameBoard = (() => {
                 if (player1.turn == true && square.innerHTML == "" && winner == null) {
                     board[element.id] = player1.mark;
                     square.textContent = player1.mark;
+                    square.style.color = "#27374d"
+                    square.style["font-size"] = "5em"
                     player1.turn = false;
                     player2.turn = true;
                 }
                 else if (player2.turn == true && square.innerHTML == "" && winner == null) {
                     board[element.id] = player2.mark;
                     square.textContent = player2.mark;
+                    square.style.color = "#526D82"
+                    square.style["font-size"] = "5em"
                     player1.turn = true;
                     player2.turn = false;
                 }
