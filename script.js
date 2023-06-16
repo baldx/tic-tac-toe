de5b585bea1777762b795ca27a3b9633342d21b4
baldx <mr.ahmad.m2006@gmail.com>
2023-06-16 14:01:06 +0200

    fix checkWinner script

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
+const createPlayer = (name, mark) => {
+    return {name, mark};
+}
+
 let gameBoard = (() => {
-    let board = [];
+    "use strict"
+
     let winner = null;
-    for (i = 0; i < 9; i++) {
+
+    let board = [];
+    for (let i = 0; i < 9; i++) {
         board.push('');
     }
-    let winPossibilities = [
-        [0, 1, 2],
-        [0, 3, 6],
-        [0, 4, 8],
-        [1, 4, 7],
-        [2, 5, 8],
-        [2, 4, 6],
-        [3, 4, 5],
-        [6, 7, 8]
-    ];
-    const createPlayer = (name, mark, turn) => {
-        return {name, mark, turn};
-    }
-    const player1 = createPlayer("player1", "X", true);
-    const player2 = createPlayer("player2", "O", false);
+
+    const player1 = createPlayer("player1", "X");
+    const player2 = createPlayer("player2", "O");
     let currentPlayer = player1;
+
+    let text = document.querySelector(".text")
 
 
     const playerTurn = (() => {
@@ -32,15 +29,19 @@ let gameBoard = (() => {
                     square.style.color = "#27374d"
                     square.style["font-size"] = "5em"
                     currentPlayer = player2;
-                    playButton.text.innerHTML = "test"
+                    text.style.display = "block";
+                    text.textContent = "Its player O's turn"
                     board.splice(index, 1, player1.mark)
                     console.log(board)
+                    checkWinner();
                 }
                 else if (player2 == currentPlayer && square.innerHTML == "" && winner == null) {
                     square.textContent = player2.mark;
                     square.style.color = "#526D82"
                     square.style["font-size"] = "5em"
                     currentPlayer = player1;
+                    text.style.display = "block";
+                    text.textContent = "Its player X's turn"
                     board.splice(index, 1, player2.mark)
                     console.log(board)
                 }
@@ -48,31 +49,76 @@ let gameBoard = (() => {
             })
         })
     })();
-    
-
-    function checkWinner() {
-        winPossibilities.forEach(item => {
-            if (board[item[0]] === this.currentPlayer.mark && board[item[1]] === this.currentPlayer.mark && board[item[2]] === this.currentPlayer.mark) {
-                console.log("winner");
-                winner = true;
-            }
-        })
-    }
-
     return {
+        board,
         playerTurn,
-        checkWinner,
+        currentPlayer,
+        player1,
+        player2,
+        winner
     }
+})();
+
+const checkWinner = () => {
+
+    let winPossibilities = [
+        [0, 1, 2],
+        [0, 3, 6],
+        [0, 4, 8],
+        [1, 4, 7],
+        [2, 5, 8],
+        [2, 4, 6],
+        [3, 4, 5],
+        [6, 7, 8]
+    ];
+
+
+    winPossibilities.forEach(index => {
+        if (gameBoard.board[index[0]] == gameBoard.currentPlayer.mark && gameBoard.board[index[1]] == gameBoard.currentPlayer.mark && gameBoard.board[index[2]] == gameBoard.currentPlayer.mark) {
+            console.log("winner")
+        }
+
+    })
+};
+
+const game = (() => {
+
+    "use strict"
+
+    let winPossibilities = [
+        [0, 1, 2],
+        [0, 3, 6],
+        [0, 4, 8],
+        [1, 4, 7],
+        [2, 5, 8],
+        [2, 4, 6],
+        [3, 4, 5],
+        [6, 7, 8]
+    ];
+
+    let text = document.querySelector(".text")
+
+
+   
+
+    function checkDraw() {
+        if (gameBoard.winner === false) {
+            text.textContent = `It's a Draw!`
+            gameBoard.winner = null;
+        }
+    }
+
+    checkDraw();
 })();
 
 
 
+const playButtonClick = (() => {
+    let playBtn = document.querySelector(".play");
+    let board = document.querySelector(".board");
+    let body = document.querySelector("body");
+    let text = document.querySelector(".text")
 
-const playButton = (() => {
-    playBtn = document.querySelector(".play");
-    board = document.querySelector(".board");
-    body = document.querySelector("body");
-    text = document.querySelector(".text")
 
     playBtn.onclick = () => {
         firstPage = document.querySelector(".first-page");
@@ -81,6 +127,8 @@ const playButton = (() => {
         board.style.display = "grid";
         body.style["justify-content"] = "center";
 
+        text.textContent = "Player X goes first";
         text.style.display = "block";
+
     }
 })();
